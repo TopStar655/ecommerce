46423ae654f4f81d5ee7e6390bda4e789af23111
TopStar <topstar2335@gmail.com>
2025-01-08 01:58:16 +0200

    fix: changed color of header

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -15,7 +15,7 @@ if ( ! isset( $wp_did_header ) ) {
 	// Set up the WordPress query.
 	wp();
 
-	$str = "asdfasdfasdfasdfasdfsadf";
+	$str = "asdfasdfasdfasdfasdfsad";
 
 	// Load the theme template.
 	require_once ABSPATH . WPINC . '/template-loader.php';
