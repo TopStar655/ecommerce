fafd7adb3c1821abd36570b5a358fd0bf5cb87f5
TopStar <topstar2335@gmail.com>
2025-01-08 02:05:20 +0200

    add: added navbar

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -15,7 +15,7 @@ if ( ! isset( $wp_did_header ) ) {
 	// Set up the WordPress query.
 	wp();
 
-	$str = "asdfasdfasdfasdfasdfsad";
+	$str = "asdfasdfasdfasdfasdfsa";
 
 	// Load the theme template.
 	require_once ABSPATH . WPINC . '/template-loader.php';
